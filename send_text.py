fe241ac7f0853ed358aab9f0232e0bf9f435c45c
Ciampa <pti887@3c15c2cf88b4.home>
2015-08-31 00:02:20 -0400

    Add ! after SMS

--- send_text.py
+++ send_text.py
@@ -5,5 +5,5 @@ account_sid = "AC0affff5dc7bd66c26e9dbd644e3f888d"
 auth_token  = "fab9de97228cd6c546e5ef74bd1d364a"
 client = rest.TwilioRestClient(account_sid, auth_token)
  
-message = client.messages.create(body="Wha happened?", to="+18046788930",  from_="+18047939984") 
+message = client.messages.create(body="Wha happened!?", to="+18046788930",  from_="+18047939984")
 print message.sid
